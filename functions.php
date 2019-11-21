6cbe5d89b315ca30565d1bbf6d868eaf83040431
Johannes Siipola <johannes@siipo.la>
2019-11-22 00:22:12 +0200

    Skip processing if uploaded file is not an image

--- functions.php
+++ functions.php
@@ -13,12 +13,16 @@ License: GPL v2 or later
 
 function csf_wp_handle_upload($array, $var)
 {
+    if ($array['type'] !== 'image/jpeg' && $array['type'] !== 'image/png') {
+        error_log('Not a JPEG or PNG file, skipping color space fixing');
+        return $array;
+    }
 
     if (!extension_loaded('imagick')) {
         error_log('Whoops, imagick is not loaded');
         return $array;
     }
-    
+
     // Color space conversion code based on cimage
     // https://github.com/mosbth/cimage/blob/cd142c58806c8edb6164a12a20e120eb7f436dfb/CImage.php#L2552
     // The MIT License (MIT)
