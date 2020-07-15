07a51b5f1968edd5348369b12035de2c8c33eb7c
Johannes Siipola <johannes.siipola@creuna.fi>
2020-07-15 07:21:10 +0300

    Refactor color space check and convert functions

--- color-space-fixer.php
+++ color-space-fixer.php
@@ -41,54 +41,46 @@ function csf_wp_handle_upload($array, $var)
         return $array;
     }
 
-    // Color space conversion code based on cimage
-    // https://github.com/mosbth/cimage/blob/cd142c58806c8edb6164a12a20e120eb7f436dfb/CImage.php#L2552
-    // The MIT License (MIT)
-    // Copyright (c) 2012 - 2016 Mikael Roos, https://mikaelroos.se, mos@dbwebb.se
-
     try {
         $path = $array['file'];
         $image = new Imagick($path);
-        $colorspace = $image->getImageColorspace();
 
-        csf_debug('Colorspace: ' . csf_get_colorspace_name($colorspace));
+        $result = csf_check_color_space($image);
 
-        $profiles = $image->getImageProfiles('*', false);
-        $has_ICC_profile = (array_search('icc', $profiles) !== false);
-        $icc_description = $image->getImageProperty('icc:description');
-
-        csf_debug('icc profile: ' . $icc_description);
-
-        if (!$has_ICC_profile) {
-            error_log("Color Space Fixer: No icc profile found, can't convert");
+        if (!$result) {
             return $array;
         }
 
-        // c2 is the Facebook's tiny sRGB profile https://pippin.gimp.org/sRGBz/ , used by Facebook and some CDNs too
-        // eg. Fastly
-        if (stripos($icc_description, 'srgb') !== false || $icc_description === 'c2') {
-            error_log("Color Space Fixer: Already a sRGB image, no need to convert");
-            return $array;
-        }
-
-        if ($colorspace !== Imagick::COLORSPACE_SRGB) {
-            csf_debug("Color space is not SRGB (eg. it's a CMYK image), converting...");
-        }
-
-        if ($colorspace === Imagick::COLORSPACE_SRGB && stripos($icc_description, 'srgb') === false) {
-            csf_debug("Color space is sRGB but ICC profile is not (eg. it's a Adobe RGB image), converting");
-        }
-
-        $sRGB_icc = file_get_contents(__DIR__ . '/icc/sRGB2014.icc');
-        $image->profileImage('icc', $sRGB_icc);
-        $image->transformImageColorspace(Imagick::COLORSPACE_SRGB);
-        $image->writeImage($path);
+        csf_fix_color_space($image, $path);
 
     } catch (Exception $e) {
         error_log('Color Space Fixer: Whoops, failed to convert image color space');
     }
 
     return $array;
+}
+
+/**
+ * Convert color space
+ * @param Imagick $image
+ * @param $path
+ */
+function csf_fix_color_space(Imagick $image, $path)
+{
+    // Color space conversion code based on cimage
+    // https://github.com/mosbth/cimage/blob/cd142c58806c8edb6164a12a20e120eb7f436dfb/CImage.php#L2552
+    // The MIT License (MIT)
+    // Copyright (c) 2012 - 2016 Mikael Roos, https://mikaelroos.se, mos@dbwebb.se
+
+    error_log("Color Space Fixer: Converting to sRGB");
+
+    $sRGB_icc = file_get_contents(__DIR__ . '/icc/sRGB2014.icc');
+    $image->profileImage('icc', $sRGB_icc);
+    $image->transformImageColorspace(Imagick::COLORSPACE_SRGB);
+
+    error_log("Color Space Fixer: Writing image");
+
+    $image->writeImage($path);
 }
 
 /**
@@ -146,3 +138,44 @@ function csf_get_colorspace_name($colorspace) {
 add_filter('wp_handle_upload', 'csf_wp_handle_upload', 10, 2);
 
 add_action('admin_notices', 'csf_admin_notices');
+
+add_action('wp_ajax_csf_get_images', 'csf_ajax_get_images');
+
+/**
+ * Check if color space should be converted, return true if should be converted, false if not
+ * @param $image
+ * @return bool
+ */
+function csf_check_color_space($image) {
+    $colorspace = $image->getImageColorspace();
+    csf_debug('Colorspace: ' . csf_get_colorspace_name($colorspace));
+
+    $profiles = $image->getImageProfiles('*', false);
+    $has_ICC_profile = (array_search('icc', $profiles) !== false);
+    $icc_description = $image->getImageProperty('icc:description');
+
+    csf_debug('icc profile: ' . $icc_description);
+
+    if (!$has_ICC_profile) {
+        error_log("Color Space Fixer: No icc profile found, can't convert");
+        return false;
+    }
+
+    // c2 is the Facebook's tiny sRGB profile https://pippin.gimp.org/sRGBz/ , used by Facebook and some CDNs too
+    // eg. Fastly
+    if (stripos($icc_description, 'srgb') !== false || $icc_description === 'c2') {
+        error_log("Color Space Fixer: Already a sRGB image, no need to convert.");
+        return false;
+    }
+
+    if ($colorspace !== Imagick::COLORSPACE_SRGB) {
+        csf_debug("Color space is not SRGB (eg. it's a CMYK image). Should be converted.");
+        return true;
+    }
+
+    if ($colorspace === Imagick::COLORSPACE_SRGB && stripos($icc_description, 'srgb') === false) {
+        csf_debug("Color space is sRGB but ICC profile is not (eg. it's a Adobe RGB image). Should be converted.");
+        return true;
+    }
+    return false;
+}
